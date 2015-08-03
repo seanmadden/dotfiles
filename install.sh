f580cf0eb35a7747a050c3dff8dd988b5efbc218
Sean Madden <smmadden@amazon.com>
2015-08-03 21:36:25 +0000

    added samba config

--- install.sh
+++ install.sh
@@ -3,3 +3,4 @@
 sudo cp -r .zlogin .zlogout .zpreztorc .zprofile .zshenv .zshrc .vimrc .viminfo .tmux.conf .zprezto .vim .ssh ~/
 sudo cp -r zlogin zlogout zpreztorc zprofvile zshenv zshrc ~/.zprezto
 sudo cp -f vimrc /etc/vimrc
+sudo cp -rf smb.conf /etc/samba/smb.conf
