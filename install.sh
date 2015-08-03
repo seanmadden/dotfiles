c36b601628f2c98dc2739f00efdf48a2ff23d7fd
Sean Madden <smmadden@amazon.com>
2015-08-03 21:26:07 +0000

    added some more files

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 
 sudo cp -r .zlogin .zlogout .zpreztorc .zprofile .zshenv .zshrc .vimrc .viminfo .tmux.conf .zprezto .vim .ssh ~/
+sudo cp -r zlogin zlogout zpreztorc zprofvile zshenv zshrc ~/.zprezto
 sudo cp -f vimrc /etc/vimrc
